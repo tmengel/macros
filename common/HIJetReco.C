0dbd9990c6bbb55f8d238eae02bb8292a2b8959a
tmengel <tmengel@vols.utk.edu>
2026-05-26 16:05:00 -0400

    hijetreco

--- common/HIJetReco.C
+++ common/HIJetReco.C
@@ -18,8 +18,8 @@
 #include <jetbackground/TowerRho.h>
 
 #include <jetbackground/SubtractTowersRho.h>
-// #include <jetbackground/DetermineTowerBackgroundv1.h>
-// #include <jetbackground/DetermineTowerBackgroundv2.h>
+#include <jetbackground/DetermineTowerBackgroundv1.h>
+#include <jetbackground/DetermineTowerBackgroundv2.h>
 
 #include <jetbase/FastJetOptions.h>
 #include <jetbase/JetReco.h>
@@ -92,6 +92,7 @@ namespace HIJETS
   // phi reweight .. should just be true without option to change
   bool do_reweight = true; // if true, use reweighting to account for flow modulation in background estimation, if false, just exclude all towers in eta strips with seeds and/or bad towers
 
+  std::string EPD_tower_node = "TOWERINFO_CALIB_SEPD";
 
   ///! sets algo of seed jets
   Jet::ALGO seed_algo  = Jet::ANTIKT;
@@ -233,6 +234,7 @@ void MakeHITowerJets()
   if ( HIJETS::do_flow == 3 )
   {
     auto * epreco = new EventPlaneReco();
+    epreco -> set_inputNode( HIJETS::EPD_tower_node );
     se -> registerSubsystem( epreco );
   }
 
@@ -247,7 +249,7 @@ void MakeHITowerJets()
   // seed settings
   FastJetOptions seed_fj_opts( { Jet::ANTIKT, 0.2, 0 } );
   std::string seed_node_name = HIJETS::algo_prefix + "_TowerInfo_HIRecoSeedsRaw_r02";
-  std::string seed_node_subname = HIJETS::algo_prefix + "_TowerInfo_HIRecoSeedsSub1_r02";
+  std::string seed_node_subname = HIJETS::algo_prefix + "_TowerInfo_HIRecoSeedsSub_r02";
   
   // input to subtraction and background estimation modules
   // defaults are retowered emcal, ihcal, emcal
@@ -330,189 +332,188 @@ void MakeHITowerJets()
 
 }
 
-// void MakeHITowerJetsv2()
-// {
+void MakeHITowerJetsv2()
+{
   
-//   int verbosity = std::max(Enable::VERBOSITY, Enable::HIJETS_VERBOSITY);
+  int verbosity = std::max(Enable::VERBOSITY, Enable::HIJETS_VERBOSITY);
 
-//   //---------------
-//   // Fun4All server
-//   //---------------
-//   auto * se = Fun4AllServer::instance();
+  //---------------
+  // Fun4All server
+  //---------------
+  auto * se = Fun4AllServer::instance();
 
-//   // need the sepd if flow = 3
-//   if ( HIJETS::do_flow == 3 )
-//   {
-//     auto * epreco = new EventPlaneReco();
-//     se -> registerSubsystem( epreco );
-//   }
+  // need the sepd if flow = 3
+  if ( HIJETS::do_flow == 3 )
+  {
+    auto * epreco = new EventPlaneReco();
+    epreco -> set_inputNode( HIJETS::EPD_tower_node );
+    se -> registerSubsystem( epreco );
+  }
 
-//   // retower emcal 
-//   auto * rcemc = new RetowerCEMC();
-//   rcemc -> set_towerNodePrefix( HIJETS::tower_prefix );
-//   rcemc -> Verbosity( verbosity );
-//   rcemc -> set_frac_cut( 0.5 );  
-//   rcemc -> set_towerinfo( true );
-//   se -> registerSubsystem( rcemc );
+  // retower emcal 
+  auto * rcemc = new RetowerCEMC();
+  rcemc -> set_towerNodePrefix( HIJETS::tower_prefix );
+  rcemc -> Verbosity( verbosity );
+  rcemc -> set_frac_cut( 0.5 );  
+  rcemc -> set_towerinfo( true );
+  se -> registerSubsystem( rcemc );
 
-//   // seed settings
-//   FastJetOptions seed_fj_opts( { HIJETS::seed_algo, HIJETS::seed_R, 0 } );
-//   std::string seed_node_name = HIJETS::algo_prefix + "_TowerInfo_HIRecoSeedsRaw_r0" + std::to_string( static_cast<int>(HIJETS::seed_R * 10) );
-//   std::string seed_node_subname = HIJETS::algo_prefix + "_TowerInfo_HIRecoSeedsSub1_r0" + std::to_string( static_cast<int>(HIJETS::seed_R * 10) );
-//   if ( HIJETS::seed_algo == Jet::KT )
-//   {
-//     seed_node_name = "kT_TowerInfo_HIRecoSeedsRaw_r0" + std::to_string( static_cast<int>(HIJETS::seed_R * 10) );
-//     seed_node_subname = "kT_TowerInfo_HIRecoSeedsSub1_r0" + std::to_string( static_cast<int>(HIJETS::seed_R * 10) );
-//   }
+  // seed settings
+  FastJetOptions seed_fj_opts( { HIJETS::seed_algo, HIJETS::seed_R, 0 } );
+  std::string seed_node_name = HIJETS::algo_prefix + "_TowerInfo_HIRecoSeedsRaw_r0" + std::to_string( static_cast<int>(HIJETS::seed_R * 10) );
+  std::string seed_node_subname = HIJETS::algo_prefix + "_TowerInfo_HIRecoSeedsSub_r0" + std::to_string( static_cast<int>(HIJETS::seed_R * 10) );
+  if ( HIJETS::seed_algo == Jet::KT )
+  {
+    seed_node_name = "kT_TowerInfo_HIRecoSeedsRaw_r0" + std::to_string( static_cast<int>(HIJETS::seed_R * 10) );
+    seed_node_subname = "kT_TowerInfo_HIRecoSeedsSub_r0" + std::to_string( static_cast<int>(HIJETS::seed_R * 10) );
+  }
 
-//   // input to subtraction and background estimation modules
-//   // defaults are retowered emcal, ihcal, emcal
-//   std::vector< Jet::SRC > raw_tower_inputs = {
-//     Jet::CEMC_TOWERINFO_RETOWER,
-//     Jet::HCALIN_TOWERINFO,
-//     Jet::HCALOUT_TOWERINFO
-//   };
+  // input to subtraction and background estimation modules
+  // defaults are retowered emcal, ihcal, emcal
+  std::vector< Jet::SRC > raw_tower_inputs = {
+    Jet::CEMC_TOWERINFO_RETOWER,
+    Jet::HCALIN_TOWERINFO,
+    Jet::HCALOUT_TOWERINFO
+  };
 
 
-//   auto * towerjetreco = new JetReco( );
-//   for ( const auto & src : raw_tower_inputs ) { towerjetreco -> add_input( HIJETS::TowerInput( src ) ); }
-//   towerjetreco -> add_algo( new FastJetAlgoSub( seed_fj_opts ) , seed_node_name );
-//   towerjetreco -> set_algo_node( HIJETS::jet_node );
-//   towerjetreco -> set_input_node( "TOWER" );
-//   towerjetreco -> Verbosity( verbosity );
-//   se -> registerSubsystem( towerjetreco );
+  auto * towerjetreco = new JetReco( );
+  for ( const auto & src : raw_tower_inputs ) { towerjetreco -> add_input( HIJETS::TowerInput( src ) ); }
+  towerjetreco -> add_algo( new FastJetAlgoSub( seed_fj_opts ) , seed_node_name );
+  towerjetreco -> set_algo_node( HIJETS::jet_node );
+  towerjetreco -> set_input_node( "TOWER" );
+  towerjetreco -> Verbosity( verbosity );
+  se -> registerSubsystem( towerjetreco );
 
-//   auto * dtb = new DetermineTowerBackgroundv1();
-//   dtb -> SetBackgroundOutputName( "TowerInfoBackground_Sub1" );
-//   if ( HIJETS::do_flow == 0 )
-//   {
-//    dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::NoFlow );
-//    dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::NoPsi2 );
-//   }
-//   else if ( HIJETS::do_flow == 1 )
-//   {
-//    dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
-//    dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::Calo );
-//   }
-//   else if ( HIJETS::do_flow == 2 )
-//   {
-//    dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
-//    dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::Truth );
-//   }
-//   else if ( HIJETS::do_flow == 3 )
-//   {
-//    dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
-//    dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::sEPD );
-//   }
-//   else if ( HIJETS::do_flow == 4 )
-//   {
-//     dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::AVG );
-//     dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::sEPD );
-//   }
-//   dtb -> SetSeedExclusionDR( HIJETS::DR );
-//   dtb -> UseReweighting( HIJETS::do_reweight );
-//   dtb -> ExcludeFullEtaFlowStrips( HIJETS::exclude_full_eta_flow_strips );
-//   dtb -> SetRenormalizeEtaStrip( HIJETS::renormalize_eta_strip );
-//   dtb -> SetDoDoubleCheckOnUE( HIJETS::do_double_check_on_UE );
-//   dtb -> SetMinTowerEnergy( HIJETS::min_tower_energy );  
-//   dtb -> SetNOmitSeeds( HIJETS::n_omit_seeds );
+  auto * dtb = new DetermineTowerBackgroundv1();
+  dtb -> SetBackgroundOutputName( "TowerInfoBackground_Sub1" );
+  if ( HIJETS::do_flow == 0 )
+  {
+   dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::NoFlow );
+   dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::NoPsi2 );
+  }
+  else if ( HIJETS::do_flow == 1 )
+  {
+   dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
+   dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::Calo );
+  }
+  else if ( HIJETS::do_flow == 2 )
+  {
+   dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
+   dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::Truth );
+  }
+  else if ( HIJETS::do_flow == 3 )
+  {
+   dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
+   dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::sEPD );
+  }
+  else if ( HIJETS::do_flow == 4 )
+  {
+    dtb -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::AVG );
+    dtb -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::sEPD );
+  }
+  dtb -> SetSeedExclusionDR( HIJETS::DR );
+  dtb -> UseReweighting( HIJETS::do_reweight );
+  dtb -> ExcludeFullEtaFlowStrips( HIJETS::exclude_full_eta_flow_strips );
+  dtb -> SetRenormalizeEtaStrip( HIJETS::renormalize_eta_strip );
+  dtb -> SetDoDoubleCheckOnUE( HIJETS::do_double_check_on_UE );
+  dtb -> SetMinTowerEnergy( HIJETS::min_tower_energy );  
+  dtb -> SetNOmitSeeds( HIJETS::n_omit_seeds );
+  dtb -> SetVertexType( HIJETS::vertex_type );
+  dtb -> SetSeedType( 0 );
+  dtb -> SetSeedJetD( 3.0 );
+  dtb -> SetSeedMaxConst( 3.0 );
+  dtb -> SetSeedJetPt( 5.0 );
+  dtb -> SetIHCAL_TowerInfoNode( HIJETS::tower_prefix + "_HCALIN" );
+  dtb -> SetOHCAL_TowerInfoNode( HIJETS::tower_prefix + "_HCALOUT" );
+  dtb -> SetCEMC_RetowerInfoNode( HIJETS::tower_prefix + "_CEMC_RETOWER" );
+  dtb -> SetSeedJetName( seed_node_name );
+  dtb -> Verbosity( verbosity );
+  se -> registerSubsystem( dtb );
 
-//   dtb -> SetVertexType( HIJETS::vertex_type );
-//   dtb -> SetSeedType( 0 );
-//   dtb -> SetSeedJetD( 3.0 );
-//   dtb -> SetSeedMaxConst( 3.0 );
-//   dtb -> SetSeedJetPt( 5.0 );
-//   dtb -> SetIHCAL_TowerInfoNode( HIJETS::tower_prefix + "_HCALIN_TOWERINFO" );
-//   dtb -> SetOHCAL_TowerInfoNode( HIJETS::tower_prefix + "_HCALOUT_TOWERINFO" );
-//   dtb -> SetCEMC_RetowerInfoNode( HIJETS::tower_prefix + "_CEMC_TOWERINFO_RETOWER" );
-//   dtb -> SetSeedJetName( seed_node_name );
-//   dtb -> Verbosity( verbosity );
-//   se -> registerSubsystem( dtb );
-
-//   auto * casj = new CopyAndSubtractJets();
-//   casj -> SetFlowModulation( HIJETS::do_flow );
-//   casj -> set_rawseed_node( seed_node_name );
-//   casj -> set_subseed_node( seed_node_subname );
-//   casj -> set_background_node( "TowerInfoBackground_Sub1" );
-//   casj -> set_jet_node( HIJETS::jet_node );
-//   casj -> set_input_node( "TOWER" );
-//   casj -> set_towerinfo( true );
-//   casj -> set_towerNodePrefix( HIJETS::tower_prefix );
-//   casj -> Verbosity( verbosity );
-//   se   -> registerSubsystem( casj );
+  auto * casj = new CopyAndSubtractJets();
+  casj -> SetFlowModulation( HIJETS::do_flow );
+  casj -> set_rawseed_node( seed_node_name );
+  casj -> set_subseed_node( seed_node_subname );
+  casj -> set_background_node( "TowerInfoBackground_Sub1" );
+  casj -> set_jet_node( HIJETS::jet_node );
+  casj -> set_input_node( "TOWER" );
+  casj -> set_towerinfo( true );
+  casj -> set_towerNodePrefix( HIJETS::tower_prefix );
+  casj -> Verbosity( verbosity );
+  se   -> registerSubsystem( casj );
   
-//   auto * dtb2 = new DetermineTowerBackgroundv1();
-//   dtb2 -> SetBackgroundOutputName( "TowerInfoBackground_Sub2" );
-//   if ( HIJETS::do_flow == 0 )
-//   {
-//    dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::NoFlow );
-//    dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::NoPsi2 );
-//   }
-//   else if ( HIJETS::do_flow == 1 )
-//   {
-//    dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
-//    dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::Calo );
-//   }
-//   else if ( HIJETS::do_flow == 2 )
-//   {
-//    dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
-//    dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::Truth );
-//   }
-//   else if ( HIJETS::do_flow == 3 )
-//   {
-//    dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
-//    dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::sEPD );
-//   }
-//   else if ( HIJETS::do_flow == 4 )
-//   {
-//     dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::AVG );
-//     dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::sEPD );
-//   }
-//   dtb2 -> SetSeedExclusionDR( HIJETS::DR );
-//   dtb2 -> UseReweighting( HIJETS::do_reweight );
-//   dtb2 -> ExcludeFullEtaFlowStrips( HIJETS::exclude_full_eta_flow_strips );
-//   dtb2 -> SetRenormalizeEtaStrip( HIJETS::renormalize_eta_strip );
-//   dtb2 -> SetDoDoubleCheckOnUE( HIJETS::do_double_check_on_UE );
-//   dtb2 -> SetMinTowerEnergy( HIJETS::min_tower_energy );
-//   dtb2 -> SetNOmitSeeds( HIJETS::n_omit_seeds );
-//   dtb2 -> SetVertexType( HIJETS::vertex_type );
-//   dtb2 -> SetSeedType( 1 );
-//   dtb2 -> SetSeedJetPt( 7.0 );
-//   dtb2 -> SetIHCAL_TowerInfoNode( HIJETS::tower_prefix + "_HCALIN_TOWERINFO" );
-//   dtb2 -> SetOHCAL_TowerInfoNode( HIJETS::tower_prefix + "_HCALOUT_TOWERINFO" );
-//   dtb2 -> SetCEMC_RetowerInfoNode( HIJETS::tower_prefix + "_CEMC_TOWERINFO_RETOWER" );
-//   dtb2 -> SetSeedJetName( seed_node_name );
-//   dtb2 -> Verbosity( verbosity );
-//   se -> registerSubsystem( dtb2 );
+  auto * dtb2 = new DetermineTowerBackgroundv1();
+  dtb2 -> SetBackgroundOutputName( "TowerInfoBackground_Sub2" );
+  if ( HIJETS::do_flow == 0 )
+  {
+   dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::NoFlow );
+   dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::NoPsi2 );
+  }
+  else if ( HIJETS::do_flow == 1 )
+  {
+   dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
+   dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::Calo );
+  }
+  else if ( HIJETS::do_flow == 2 )
+  {
+   dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
+   dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::Truth );
+  }
+  else if ( HIJETS::do_flow == 3 )
+  {
+   dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::EBE );
+   dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::sEPD );
+  }
+  else if ( HIJETS::do_flow == 4 )
+  {
+    dtb2 -> SetFlowMode( DetermineTowerBackgroundv1::FlowMode::AVG );
+    dtb2 -> SetPsi2Mode( DetermineTowerBackgroundv1::Psi2Mode::sEPD );
+  }
+  dtb2 -> SetSeedExclusionDR( HIJETS::DR );
+  dtb2 -> UseReweighting( HIJETS::do_reweight );
+  dtb2 -> ExcludeFullEtaFlowStrips( HIJETS::exclude_full_eta_flow_strips );
+  dtb2 -> SetRenormalizeEtaStrip( HIJETS::renormalize_eta_strip );
+  dtb2 -> SetDoDoubleCheckOnUE( HIJETS::do_double_check_on_UE );
+  dtb2 -> SetMinTowerEnergy( HIJETS::min_tower_energy );
+  dtb2 -> SetNOmitSeeds( HIJETS::n_omit_seeds );
+  dtb2 -> SetVertexType( HIJETS::vertex_type );
+  dtb2 -> SetSeedType( 1 );
+  dtb2 -> SetSeedJetPt( 7.0 );
+  dtb2 -> SetIHCAL_TowerInfoNode( HIJETS::tower_prefix + "_HCALIN" );
+  dtb2 -> SetOHCAL_TowerInfoNode( HIJETS::tower_prefix + "_HCALOUT" );
+  dtb2 -> SetCEMC_RetowerInfoNode( HIJETS::tower_prefix + "_CEMC_RETOWER" );
+  dtb2 -> SetSeedJetName( seed_node_name );
+  dtb2 -> Verbosity( verbosity );
+  se -> registerSubsystem( dtb2 );
 
-//   auto * st = new SubtractTowers( );
-//   st -> SetFlowModulation( HIJETS::do_flow );
-//   st -> Verbosity( verbosity );
-//   st -> set_towerinfo( true );
-//   st -> set_towerNodePrefix( HIJETS::tower_prefix );
-//   se -> registerSubsystem( st );
+  auto * st = new SubtractTowers( );
+  st -> SetFlowModulation( HIJETS::do_flow );
+  st -> Verbosity( verbosity );
+  st -> set_towerinfo( true );
+  st -> set_towerNodePrefix( HIJETS::tower_prefix );
+  se -> registerSubsystem( st );
 
 
-//   // input to jet reconstruction (after subtraction)
-//   std::vector< Jet::SRC > sub_tower_inputs = {
-//     Jet::CEMC_TOWERINFO_SUB1,
-//     Jet::HCALIN_TOWERINFO_SUB1,
-//     Jet::HCALOUT_TOWERINFO_SUB1
-//   };
-//   std::vector< float > jet_Rs = { 0.2, 0.3, 0.4, 0.5 };
+  // input to jet reconstruction (after subtraction)
+  std::vector< Jet::SRC > sub_tower_inputs = {
+    Jet::CEMC_TOWERINFO_SUB1,
+    Jet::HCALIN_TOWERINFO_SUB1,
+    Jet::HCALOUT_TOWERINFO_SUB1
+  };
+  std::vector< float > jet_Rs = { 0.2, 0.3, 0.4, 0.5 };
   
-//   towerjetreco = new JetReco( );
-//   for ( const auto & src : sub_tower_inputs ) { towerjetreco -> add_input( HIJETS::TowerInput( src ) ); }
-//   for ( const auto &R : jet_Rs ) { towerjetreco -> add_algo( HIJETS::GetFJAlgo(R), HIJETS::algo_prefix + "_Tower_r0" + std::to_string( static_cast<int>(R * 10) ) + "_Sub1" ); }
-//   towerjetreco -> set_algo_node( HIJETS::jet_node );
-//   towerjetreco -> set_input_node( "TOWER" );
-//   towerjetreco -> Verbosity( verbosity );
-//   se -> registerSubsystem( towerjetreco );
+  towerjetreco = new JetReco( );
+  for ( const auto & src : sub_tower_inputs ) { towerjetreco -> add_input( HIJETS::TowerInput( src ) ); }
+  for ( const auto &R : jet_Rs ) { towerjetreco -> add_algo( HIJETS::GetFJAlgo(R), HIJETS::algo_prefix + "_Tower_r0" + std::to_string( static_cast<int>(R * 10) ) + "_Sub1" ); }
+  towerjetreco -> set_algo_node( HIJETS::jet_node );
+  towerjetreco -> set_input_node( "TOWER" );
+  towerjetreco -> Verbosity( verbosity );
+  se -> registerSubsystem( towerjetreco );
 
-//   return;
+  return;
 
-// }
-
+}
 
 // ----------------------------------------------------------------------------
 //! Make jets out of tracks with background subtraction
@@ -594,7 +595,7 @@ void HIJetReco()
 
   // run approriate jet reconstruction routines
   if (Enable::HIJETS_TOWER && !Enable::HIJETS_ENABLE_TEST ) MakeHITowerJets();
-  // if (Enable::HIJETS_TOWER && Enable::HIJETS_ENABLE_TEST ) MakeHITowerJetsv2();
+  if (Enable::HIJETS_TOWER && Enable::HIJETS_ENABLE_TEST ) MakeHITowerJetsv2();
   if (Enable::HIJETS_TRACK) MakeHITrackJets();
   if (Enable::HIJETS_PFLOW) MakeHIPFlowJets();
 }
